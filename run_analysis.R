85841554f538a096c61389a5cddd91fdc2d74d80
fallenazra3l <tigerboyaa@gmail.com>
2023-01-03 21:00:17 -0500

    file reading, first steps

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-library(magrittr)
+library(dplyr)
 
 path1 <- "./UCI HAR Dataset/"
 path2 <- "test/"
@@ -29,8 +29,31 @@ features <- read.table(newFile)
 newFile <- file(paste0(path1, "activity_labels.txt"))
 activityLabels <- read.table(newFile)
 
+# add row subject ids, column names to x vars
+names(xtest) <- features[,2]
+names(xtrain) <- features[,2]
 
+# Cbind the yvars and subject ID's
+xtest <- cbind(subject_test[,1], xtest) %>%
+  cbind(ytest[,1])
+xtrain <- cbind(subject_train[,1], xtrain) %>%
+  cbind(ytrain[,1])
+
+# determine how to reasonably merge xtest and xtrain, etc
+
+# DO NOT USE MERGE UNLESS YOU CAN ENSURE ORDER REMAINS STABLE
+
+
+# convert yvars into activity names and add a new column
+
+# remove all columns not involving mean, std. Keep the activity column
+
+# check the spec again at this point because wtf is this project and its steps
+
+# create another dataset for average of every variable, all participants
 
 # print(str(features))
 # print(str(activityLabels))
 print(str(subject_train))
+
+
